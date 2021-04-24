4ccea2951f2698633ff896d5a2d5b7932202934d
patelamancode <74897715+patelamancode@users.noreply.github.com>
2021-04-24 22:43:51 +0530

    update changes and remove modal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Title from './Components/Title';
 import UploadForm from './Components/UploadForm';
 import ImageGrid from './Components/ImageGrid';
-import Modal from './Components/Modal';
+
 
 
 
 function App() {
-  const [selectedImg, setSelectedImg] = useState(null);
+ 
   return(
     <div className='App'>
        <Title />
        <UploadForm />
-       <ImageGrid  setSelectedImg={ setSelectedImg}/>
-       { selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg}/>}
+       <ImageGrid />
     </div>
     
   )
--- src/Components/ImageGrid.js
+++ src/Components/ImageGrid.js
@@ -2,7 +2,7 @@ import React from 'react';
 import useFirestore from '../hooks/useFirestore';
 import {motion} from "framer-motion";
 
-const ImageGrid = ({setSelectedImg}) => {
+const ImageGrid = () => {
     const {docs} = useFirestore('images');
     console.log(docs);
 
@@ -13,8 +13,7 @@ const ImageGrid = ({setSelectedImg}) => {
                 <motion.div
                    className="img-wrap" key={doc.id}
                    layout
-                   whileHover={{ opacity:1.3}}
-                   onClick={()=> setSelectedImg(doc.url)}
+                   whileHover={{ opacity:1.5}}
                   >
                   <img src={doc.url} alt="uploading images"/>
                 </motion.div>
